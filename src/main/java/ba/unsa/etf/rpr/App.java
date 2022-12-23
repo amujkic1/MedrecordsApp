c092be3e771fd431877b41802c93a0c87a322c2c
amujkic1 <amujkic1@etf.unsa.ba>
2022-12-23 20:29:35 +0100

    App class

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,5 +1,16 @@
 package ba.unsa.etf.rpr;
 
+import ba.unsa.etf.rpr.dao.*;
+import ba.unsa.etf.rpr.domain.Doctors;
+import ba.unsa.etf.rpr.domain.Patients;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.sql.Date;
+import java.util.Properties;
+
 /**
  * Hello world!
  *
@@ -8,6 +19,26 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        DoctorsDao doc = new DoctorsDaoImpl();
+        Doctors newdoc = new Doctors(11, "Steven", "Bennet", "2376 Alpha Avenue",
+                "sbennet@gmail.com", "270-415-4762", 40, "m", "pediatrist",
+                "acfe345wdr", "StevenBennet");
+
+
+        //doc.add(newdoc);
+        //doc.delete(11);
+
+        /*Properties database_prop = new Properties();
+        InputStream resource = null;
+        try {
+            resource = App.class.getResource("/database.properties").openStream();
+            database_prop.load(resource);
+            System.out.println(database_prop.getProperty("username"));
+            System.out.println(database_prop.getProperty("password"));
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }*/
     }
 }
