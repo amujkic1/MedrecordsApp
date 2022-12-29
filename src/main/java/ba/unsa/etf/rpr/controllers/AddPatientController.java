174f38cec2ac157b760d8ccabc0b8fe2bbb88111
amujkic1 <amujkic1@etf.unsa.ba>
2022-12-29 17:07:56 +0100

    addPatientToBase method

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/AddPatientController.java
@@ -0,0 +1,106 @@
+package ba.unsa.etf.rpr.controllers;
+
+//napravi metodu generatId koja daje sljedeci moguci id u tabeli na nacin da vrati preko fje COUNT broj redova i da sljedeci
+// provjera da li postoji zadani id za svaki slucaj
+
+import ba.unsa.etf.rpr.dao.PatientsDaoImpl;
+import ba.unsa.etf.rpr.domain.Patients;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.*;
+import java.util.Properties;
+import java.util.ResourceBundle;
+
+public class AddPatientController implements Initializable {
+
+    @FXML
+    private BorderPane bp;
+    @FXML
+    private Label first_name;
+    @FXML
+    private TextField first_name_box;
+    @FXML
+    private Label last_name;
+    @FXML
+    private TextField last_name_box;
+    @FXML
+    private TextField address_box;
+    @FXML
+    private Label address;
+    @FXML
+    private TextField email_box;
+    @FXML
+    private Label email;
+    @FXML
+    private TextField telephone_box;
+    @FXML
+    private Label telephone;
+    @FXML
+    private TextField age_box;
+    @FXML
+    private Label age;
+    @FXML
+    private TextField gender_box;
+    @FXML
+    private Label gender;
+    @FXML
+    private TextField record_id_box;
+    @FXML
+    private Label record_id;
+    @FXML
+    private TextField username_box;
+    @FXML
+    private Label username;
+    @FXML
+    private TextField password_box;
+    @FXML
+    private Label password;
+    @FXML
+    private Button addbttn;
+    @FXML
+    private Button cancelbttn;
+private Connection conn;
+
+    public AddPatientController() {
+        Properties dbProp = new Properties();
+        try{
+            dbProp.load(PatientsDaoImpl.class.getResource("/database.properties").openStream());
+            conn = DriverManager.getConnection(dbProp.getProperty("url"), dbProp.getProperty("username"), dbProp.getProperty("password"));
+        }catch (IOException e){
+            e.printStackTrace();
+        }catch (SQLException sqle){
+            System.out.println(sqle.getErrorCode());
+        }
+    }
+
+    public void addPatientToBase(){
+        PatientsDaoImpl p = new PatientsDaoImpl();
+
+        Patients patient = new Patients(generateId(), first_name_box.getText(), last_name_box.getText(),
+                address_box.getText(), email_box.getText(), telephone_box.getText(), Integer.parseInt(age_box.getText()),
+                gender_box.getText(), Integer.parseInt(record_id_box.getText()), password_box.getText(),
+                username_box.getText(), 8736);
+
+        p.add(patient);
+
+    }
+
+    public int generateId(){
+        PatientsDaoImpl ptdao = new PatientsDaoImpl();
+        int num = ptdao.numberOfRows();
+        return num+1;
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
