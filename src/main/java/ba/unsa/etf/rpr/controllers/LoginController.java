3170b85460f7e3a41dea9b70e34578c18315a894
amujkic1 <amujkic1@etf.unsa.ba>
2023-01-06 23:27:22 +0100

    changes in switching scenes

--- src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
@@ -2,8 +2,10 @@ package ba.unsa.etf.rpr.controllers;
 
 import ba.unsa.etf.rpr.MainFX;
 import ba.unsa.etf.rpr.dao.DoctorsDaoImpl;
+import ba.unsa.etf.rpr.dao.PatientsDaoImpl;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -16,6 +18,9 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+import static javafx.scene.layout.Region.USE_PREF_SIZE;
 
 public class LoginController implements Initializable {
 
@@ -42,19 +47,38 @@ public class LoginController implements Initializable {
     public void validateLogin() throws IOException {
 
         DoctorsDaoImpl doc = new DoctorsDaoImpl();
+        PatientsDaoImpl pt = new PatientsDaoImpl();
         MainFX m = new MainFX();
+        String user = username.getText();
 
         if(doc.searchByUsername(username.getText()) != null
                 && doc.searchByUsername(username.getText()).getPassword().equals(password.getText())){
             if(choice.getValue().equals("Doctor")){
                 System.out.println("doctor");
             }
-            //System.out.println("Success");
-            m.switchScenes("/fxml/afterdoctorlogin.fxml");
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/afterdoctorlogin.fxml"));
+            AfterDoctorLoginController afterDoctorLoginController = new AfterDoctorLoginController(username.getText());
+            fxmlLoader.setController(afterDoctorLoginController);
+            Parent root = fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root, USE_PREF_SIZE, USE_PREF_SIZE));
+            stage.show();
+            Stage s = (Stage)username.getScene().getWindow();
+            s.close();
         }
+
+        else if(pt.searchByUsername(username.getText()) != null
+                && pt.searchByUsername(username.getText()).getPassword().equals(password.getText())){
+            if(choice.getValue().equals("Patient")){
+                System.out.println("patient");
+                //m.switchScenes("/fxml/afterpatientlogin.fxml");
+            }
+        }
+
         else if(username.getText().isEmpty() && password.getText().isEmpty()){
             wronglogin.setText("Please enter your data");
         }
+
         else{
             wronglogin.setText("Wrong username or password");
         }
@@ -62,7 +86,10 @@ public class LoginController implements Initializable {
     }
 
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
         choice.getItems().addAll("Doctor", "Patient");
     }
+
+
 }
