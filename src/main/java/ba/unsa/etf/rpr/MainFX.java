a791d2a2ac41eda5079c23980288dd793bdf763a
amujkic1 <amujkic1@etf.unsa.ba>
2023-01-06 23:26:05 +0100

    removed switchScenes method

--- src/main/java/ba/unsa/etf/rpr/MainFX.java
+++ src/main/java/ba/unsa/etf/rpr/MainFX.java
@@ -1,7 +1,6 @@
 package ba.unsa.etf.rpr;
-
-import ba.unsa.etf.rpr.controllers.AfterLoginController;
-import ba.unsa.etf.rpr.controllers.LoginController;
+import ba.unsa.etf.rpr.dao.PatientsDaoImpl;
+import ba.unsa.etf.rpr.domain.Patients;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -11,34 +10,46 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+import static javafx.scene.layout.Region.USE_PREF_SIZE;
 
 public class MainFX extends Application {
-    private static Stage stg;
+    public static Stage stg;
+    private Scene scene;
     @Override
     public void start(Stage stage) throws Exception {
-        stg = stage;
+        stg=stage;
+        stg.setResizable(false);
+        //stage.setResizable(false);
 
-        /*Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("/fxml/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
         stage.setTitle("medrecords");
-        stage.setScene(new Scene(root,600,400));
-        stage.show();*/
+        stage.setScene(new Scene(root,USE_PREF_SIZE,USE_PREF_SIZE));
+        stage.show();
 
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/sample.fxml"));
+
+        /*Patients patient = new Patients(123, "A", "B",
+                "C", "D", "E", 44,
+                "f", 345, "asd",
+                "www", 7896);
+
+        p.add(patient);*/
+
+        /*FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/sample.fxml"));
         LoginController loginController = new LoginController();
         fxmlLoader.setController(loginController);
         Parent root = fxmlLoader.load();
         stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
         stage.setTitle("Med-records");
-        stage.show();
+        stage.show();*/
     }
 
-    /*public void changeScene(String fxml) throws IOException {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    /*public void switchScenes(String fxml) throws IOException {
         Parent pane = FXMLLoader.load(getClass().getResource(fxml));
         stg.getScene().setRoot(pane);
     }*/
-
-    public static void main(String[] args) {
-        launch();
-    }
-
 }
