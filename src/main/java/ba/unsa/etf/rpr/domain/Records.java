e5d2d4496023107138b0497d165d35c5ab985770
amujkic1 <amujkic1@etf.unsa.ba>
2023-01-11 22:27:03 +0100

    updates in Records class

--- src/main/java/ba/unsa/etf/rpr/domain/Records.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Records.java
@@ -2,27 +2,27 @@ package ba.unsa.etf.rpr.domain;
 
 import java.sql.Date;
 
-public class Records{
+public class Records implements Idable {
     private int record_id;
     private int patient_id;
     private int doctor_id;
     private String diagnosis;
-    private Date first_checkup_date;
+    private String allergies;
+    private String treatments;
+    private Double height;
+    private Double weight;
 
-    public Records(int record_id, int patient_id, int doctor_id, String diagnosis, Date first_checkup_date) {
+    public Records(){}
+    public Records(int record_id, int patient_id, int doctor_id, String diagnosis, String allergies, String treatments,
+                   Double height, Double weight) {
         this.record_id = record_id;
         this.patient_id = patient_id;
         this.doctor_id = doctor_id;
         this.diagnosis = diagnosis;
-        this.first_checkup_date = first_checkup_date;
-    }
-
-    public int getRecord_id() {
-        return record_id;
-    }
-
-    public void setRecord_id(int record_id) {
-        this.record_id = record_id;
+        this.allergies = allergies;
+        this.treatments = treatments;
+        this.height = height;
+        this.weight = weight;
     }
 
     public int getPatient_id() {
@@ -49,11 +49,45 @@ public class Records{
         this.diagnosis = diagnosis;
     }
 
-    public Date getFirst_checkup_date() {
-        return first_checkup_date;
+    public String getAllergies() {
+        return allergies;
     }
 
-    public void setFirst_checkup_date(Date first_checkup_date) {
-        this.first_checkup_date = first_checkup_date;
+    public void setAllergies(String allergies) {
+        this.allergies = allergies;
+    }
+
+    public String getTreatments() {
+        return treatments;
+    }
+
+    public void setTreatments(String treatments) {
+        this.treatments = treatments;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+
+    public void setHeight(Double height) {
+        this.height = height;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public void setId(int id) {
+        this.record_id = record_id;
+    }
+
+    @Override
+    public int getId() {
+        return record_id;
     }
 }
