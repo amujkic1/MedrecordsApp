83e69f677030fe6908a57ccac9f85a550600125a
amujkic1 <amujkic1@etf.unsa.ba>
2023-01-11 22:23:36 +0100

    updates in AfterPatientLoginController

--- src/main/java/ba/unsa/etf/rpr/controllers/AfterPatientLoginController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AfterPatientLoginController.java
@@ -1,4 +1,9 @@
 package ba.unsa.etf.rpr.controllers;
-
 public class AfterPatientLoginController {
+
+    String patientUsername;
+
+    public AfterPatientLoginController(String patientUsername) {
+        this.patientUsername = patientUsername;
+    }
 }
