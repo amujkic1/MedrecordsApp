4f1665095484e2f456e9cd257b120194169cfc3d
amujkic1 <amujkic1@etf.unsa.ba>
2023-01-17 22:03:37 +0100

    changes in RecordsDaoImpl class

--- src/main/java/ba/unsa/etf/rpr/dao/RecordsDaoImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/RecordsDaoImpl.java
@@ -1,5 +1,6 @@
 package ba.unsa.etf.rpr.dao;
 
+import ba.unsa.etf.rpr.domain.Doctors;
 import ba.unsa.etf.rpr.domain.Records;
 
 import java.io.FileReader;
@@ -11,19 +12,9 @@ import java.util.TreeMap;
 
 public class RecordsDaoImpl extends AbstractDao<Records> implements RecordsDao {
 
-    //private Connection conn;
     private static RecordsDaoImpl instance = null;
     public RecordsDaoImpl(){
         super("RECORDS");
-        /*Properties dbProp = new Properties();
-        try{
-            dbProp.load(RecordsDaoImpl.class.getResource("/database.properties").openStream());
-            conn = DriverManager.getConnection(dbProp.getProperty("url"), dbProp.getProperty("username"), dbProp.getProperty("password"));
-        }catch (IOException ioe){
-            ioe.printStackTrace();
-        }catch(SQLException sqle){
-            System.out.println(sqle.getErrorCode());
-        }*/
     }
 
     public static RecordsDaoImpl getInstance(){
@@ -37,43 +28,24 @@ public class RecordsDaoImpl extends AbstractDao<Records> implements RecordsDao {
             instance=null;
     }
 
-    /*@Override
-    public void add(Records item) {
-        String query = "INSERT INTO RECORDS(id, patient_id, doctor_id, diagnosis, first_checkup_date)" +
-                "VALUES(?,?,?,?,?)";
+    public Records findUserRecord(String username){
+        Records rec = null;
         try{
-        PreparedStatement stmt = conn.prepareStatement(query);
-        stmt.setInt(1,item.getRecord_id());
-        stmt.setInt(2,item.getPatient_id());
-        stmt.setInt(3,item.getDoctor_id());
-        stmt.setString(4,item.getDiagnosis());
-        stmt.setDate(5,item.getFirst_checkup_date());
-        stmt.executeUpdate();
-        stmt.close();
-        }catch(SQLException sqle){
-            System.out.println(sqle.getErrorCode());
-        }
-
-    }
-
-    @Override
-    public void update(Records item, int id) {
-        String query = "UPDATE RECORDS SET id = ?, patient_id = ?, doctor_id = ?" +
-                "diagnosis = ?, first_checkup_date = ?" +
-                "WHERE record_id = " + id;
-        try{
-            PreparedStatement stmt = conn.prepareStatement(query);
-            stmt.setInt(1,item.getRecord_id());
-            stmt.setInt(2,item.getPatient_id());
-            stmt.setInt(3,item.getDoctor_id());
-            stmt.setString(4,item.getDiagnosis());
-            stmt.setDate(5,item.getFirst_checkup_date());
-            stmt.executeUpdate();
-            stmt.close();
+            Statement stmt = getConnection().createStatement();
+            ResultSet res = stmt.executeQuery("SELECT * FROM RECORDS WHERE username = '" + username + "'");
+            while(res.next()){
+                rec = new Records(res.getInt("id"), res.getInt("patient_id"),
+                        res.getInt("doctor_id"), res.getString("diagnosis"),
+                        res.getString("allergies"), res.getString("treatments"),
+                        res.getDouble("height"), res.getDouble("weight"));
+            }
         }catch (SQLException sqle){
             System.out.println(sqle.getErrorCode());
         }
-    }*/
+        return rec;
+    }
+
+
 
     @Override
     public Records rowToObject(ResultSet rs) {
@@ -108,33 +80,5 @@ public class RecordsDaoImpl extends AbstractDao<Records> implements RecordsDao {
         return row;
     }
 
-    /*@Override
-    public Records getById(int id) {
-        Records r = null;
-        try{
-            Statement stmt = conn.createStatement();
-            ResultSet res = stmt.executeQuery("SELECT * FROM RECORDS WHERE id = " + id);
-            while(res.next()){
-                r = new Records(res.getInt("id"), res.getInt("patient_id"),
-                        res.getInt("doctor_id"), res.getString("diagnosis"),
-                        res.getDate("first_checkup_date")) {
-                };
-            }
-        }catch (SQLException sqle){
-            System.out.println(sqle.getErrorCode());
-        }
-        return r;
-    }
 
-    @Override
-    public void delete(int id) {
-        String query = "DELETE FROM RECORDS WHERE id = " + id;
-        try {
-            PreparedStatement stmt = conn.prepareStatement(query);
-            stmt.executeUpdate();
-            stmt.close();
-        }catch (SQLException sqle){
-            System.out.println(sqle.getErrorCode());
-        }
-    }*/
 }
