a0a384e58f4e4561e89d7844264444152fbb2e16
amujkic1 <amujkic1@etf.unsa.ba>
2023-01-19 09:50:37 +0100

    changes in PatientRecordController class

--- src/main/java/ba/unsa/etf/rpr/controllers/PatientRecordController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/PatientRecordController.java
@@ -1,5 +1,6 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.dao.RecordsDaoImpl;
 import ba.unsa.etf.rpr.domain.Patients;
 import ba.unsa.etf.rpr.domain.Records;
 import javafx.fxml.FXML;
@@ -12,15 +13,27 @@ import java.util.ResourceBundle;
 public class PatientRecordController implements Initializable {
     @FXML
     private Label name;
+    @FXML
+    private Label blood;
+    @FXML
+    private Label height;
+    @FXML
+    private Label weight;
     private Patients patient;
     private Records rec;
-
-    public PatientRecordController(Patients patient) {
+    private RecordsDaoImpl tmp;
+    private int record_id;
+    public PatientRecordController(Patients patient, Records rec) {
         this.patient = patient;
+        this.rec = rec;
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         name.setText(patient.toString());
+        blood.setText(rec.getBlood());
+        height.setText(rec.getHeight()+"cm");
+        weight.setText(rec.getWeight()+"kq");
+
     }
 }
