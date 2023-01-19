964ef0835cce0269261a64444f5dd8cfff792c0a
amujkic1 <amujkic1@etf.unsa.ba>
2023-01-19 09:51:56 +0100

    changes in Records class

--- src/main/java/ba/unsa/etf/rpr/domain/Records.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Records.java
@@ -3,26 +3,36 @@ package ba.unsa.etf.rpr.domain;
 import java.sql.Date;
 
 public class Records implements Idable {
-    private int record_id;
+    private int id;
     private int patient_id;
     private int doctor_id;
     private String diagnosis;
     private String allergies;
-    private String treatments;
+    private String prescriptions;
     private Double height;
     private Double weight;
+    private String blood;
+
+    public String getBlood() {
+        return blood;
+    }
+
+    public void setBlood(String blood) {
+        this.blood = blood;
+    }
 
     public Records(){}
-    public Records(int record_id, int patient_id, int doctor_id, String diagnosis, String allergies, String treatments,
-                   Double height, Double weight) {
-        this.record_id = record_id;
+    public Records(int id, int patient_id, int doctor_id, String diagnosis, String allergies, String prescriptions,
+                   Double height, Double weight, String blood) {
+        this.id = id;
         this.patient_id = patient_id;
         this.doctor_id = doctor_id;
         this.diagnosis = diagnosis;
         this.allergies = allergies;
-        this.treatments = treatments;
+        this.prescriptions = prescriptions;
         this.height = height;
         this.weight = weight;
+        this.blood = blood;
     }
 
     public int getPatient_id() {
@@ -57,12 +67,12 @@ public class Records implements Idable {
         this.allergies = allergies;
     }
 
-    public String getTreatments() {
-        return treatments;
+    public String getPrescriptions() {
+        return prescriptions;
     }
 
-    public void setTreatments(String treatments) {
-        this.treatments = treatments;
+    public void setPrescriptions(String treatments) {
+        this.prescriptions = treatments;
     }
 
     public Double getHeight() {
@@ -83,11 +93,11 @@ public class Records implements Idable {
 
     @Override
     public void setId(int id) {
-        this.record_id = record_id;
+        this.id = id;
     }
 
     @Override
     public int getId() {
-        return record_id;
+        return id;
     }
 }
