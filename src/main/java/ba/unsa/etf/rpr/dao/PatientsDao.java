343e598dd6ef529a6257861c811dc1aa3399bcbf
amujkic1 <amujkic1@etf.unsa.ba>
2023-01-25 21:56:38 +0100

    changes in PatientsDao class

--- src/main/java/ba/unsa/etf/rpr/dao/PatientsDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/PatientsDao.java
@@ -1,9 +1,14 @@
 package ba.unsa.etf.rpr.dao;
 
-import ba.unsa.etf.rpr.dao.Dao;
 import ba.unsa.etf.rpr.domain.Patients;
+import javafx.collections.ObservableList;
+
+import java.sql.SQLException;
 
 public interface PatientsDao extends Dao<Patients> {
     Patients findByUsername(String username);
     int numberOfRows();
+    Patients searchByUsername(String username);
+
+    public ObservableList<Patients> allPatients(int docId) throws SQLException;
 }
