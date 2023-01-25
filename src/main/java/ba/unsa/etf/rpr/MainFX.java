88aed1f942c84ae89014ade8a69ec47abc20e2c3
amujkic1 <amujkic1@etf.unsa.ba>
2023-01-25 21:50:51 +0100

    added icon to header

--- src/main/java/ba/unsa/etf/rpr/MainFX.java
+++ src/main/java/ba/unsa/etf/rpr/MainFX.java
@@ -1,15 +1,12 @@
 package ba.unsa.etf.rpr;
-import ba.unsa.etf.rpr.dao.PatientsDaoImpl;
-import ba.unsa.etf.rpr.domain.Patients;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
-import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 import static javafx.scene.layout.Region.USE_PREF_SIZE;
 
 public class MainFX extends Application {
@@ -19,28 +16,19 @@ public class MainFX extends Application {
     public void start(Stage stage) throws Exception {
         stg=stage;
         stg.setResizable(false);
-        //stage.setResizable(false);
 
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
         stage.setTitle("medrecords");
+        stage.getIcons().add(new Image(MainFX.class.getResourceAsStream("/img/main_icon.png")));
         stage.setScene(new Scene(root,USE_PREF_SIZE,USE_PREF_SIZE));
         stage.show();
 
+        /*System.out.println("test");
+        Doctors doc = new Doctors(1,"a", "b", "c", "d", "123", 22, "f",
+                "e", "d", "d");
+        DoctorManager dm = new DoctorManager();
+        dm.add(doc);*/
 
-        /*Patients patient = new Patients(123, "A", "B",
-                "C", "D", "E", 44,
-                "f", 345, "asd",
-                "www", 7896);
-
-        p.add(patient);*/
-
-        /*FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/sample.fxml"));
-        LoginController loginController = new LoginController();
-        fxmlLoader.setController(loginController);
-        Parent root = fxmlLoader.load();
-        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
-        stage.setTitle("Med-records");
-        stage.show();*/
     }
 
 
@@ -48,8 +36,4 @@ public class MainFX extends Application {
         launch(args);
     }
 
-    /*public void switchScenes(String fxml) throws IOException {
-        Parent pane = FXMLLoader.load(getClass().getResource(fxml));
-        stg.getScene().setRoot(pane);
-    }*/
 }
