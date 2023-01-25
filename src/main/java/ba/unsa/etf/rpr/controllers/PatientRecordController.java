cae83c6cf55d3daf0d5215b2c92b0df183b8f4a1
amujkic1 <amujkic1@etf.unsa.ba>
2023-01-25 21:50:20 +0100

    added back() method to handle back button

--- src/main/java/ba/unsa/etf/rpr/controllers/PatientRecordController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/PatientRecordController.java
@@ -1,14 +1,23 @@
 package ba.unsa.etf.rpr.controllers;
 
 import ba.unsa.etf.rpr.dao.RecordsDaoImpl;
+import ba.unsa.etf.rpr.domain.Doctors;
 import ba.unsa.etf.rpr.domain.Patients;
 import ba.unsa.etf.rpr.domain.Records;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import static javafx.scene.layout.Region.USE_PREF_SIZE;
 
 public class PatientRecordController implements Initializable {
     @FXML
@@ -19,14 +28,17 @@ public class PatientRecordController implements Initializable {
     private Label height;
     @FXML
     private Label weight;
+    @FXML
+    private Button tbBack;
     private Patients patient;
     private Records rec;
     private RecordsDaoImpl tmp;
     private int record_id;
-    public PatientRecordController(Patients patient, Records rec) {
+    private Doctors doctor;
+    public PatientRecordController(Patients patient, Records rec, Doctors doc) {
         this.patient = patient;
         this.rec = rec;
-    }
+        this.doctor = doc; }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -35,5 +47,18 @@ public class PatientRecordController implements Initializable {
         height.setText(rec.getHeight()+"cm");
         weight.setText(rec.getWeight()+"kq");
 
+    }
+
+    public void back() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/afterdoctorlogin.fxml"));
+        AfterDoctorLoginController afterDoctorLoginController = new AfterDoctorLoginController(doctor.getUsername(),
+                doctor.getId());
+        fxmlLoader.setController(afterDoctorLoginController);
+        Parent root = fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root, USE_PREF_SIZE, USE_PREF_SIZE));
+        stage.show();
+        Stage s = (Stage) name.getScene().getWindow();
+        s.close();
     }
 }
