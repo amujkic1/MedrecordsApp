c0dcdfb01fb5ab3b35cb5155db9f5e682d90703c
amujkic1 <amujkic1@etf.unsa.ba>
2023-02-06 13:38:16 +0100

    builder design refactoring in Doctors class

--- src/main/java/ba/unsa/etf/rpr/domain/Doctors.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Doctors.java
@@ -1,133 +1,142 @@
 package ba.unsa.etf.rpr.domain;
 
 public class Doctors implements Idable {
-    private int doctor_id;
-    private String first_name;
-    private String last_name;
-    private String address;
-    private String email;
-    private String telephone;
-    private int age;
-    private String gender;
-    private String specialization;
-    private String password;
-    private String username;
+    //required parameters
+    private int id;
+    private final String password;
+    private final String username;
+    //optional parameters
+    private final String first_name;
+    private final String last_name;
+    private final String address;
+    private final String email;
+    private final String telephone;
+    private final int age;
+    private final String gender;
+    private final String specialization;
 
-
-    public Doctors(int doctor_id, String first_name, String last_name, String address, String email, String telephone,
-                   int age, String gender, String specialization, String password, String username) {
-        this.doctor_id = doctor_id;
-        this.first_name = first_name;
-        this.last_name = last_name;
-        this.address = address;
-        this.email = email;
-        this.telephone = telephone;
-        this.age = age;
-        this.gender = gender;
-        this.specialization = specialization;
-        this.password = password;
-        this.username = username;
-    }
-
-
-    public Doctors(){}
-
-    public int getDoctor_id() {
-        return doctor_id;
-    }
-
-    public void setDoctor_id(int doctor_id) {
-        this.doctor_id = doctor_id;
-    }
+    //public Doctors(){}
 
     public String getFirst_name() {
         return first_name;
     }
-
-    public void setFirst_name(String first_name) {
-        this.first_name = first_name;
-    }
-
     public String getLast_name() {
         return last_name;
     }
-
-    public void setLast_name(String last_name) {
-        this.last_name = last_name;
-    }
-
     public String getAddress() {
         return address;
     }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
     public String getEmail() {
         return email;
     }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getTelephone() {
         return telephone;
     }
-
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
-
     public int getAge() {
         return age;
     }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
     public String getSpecialization() {
         return specialization;
     }
-
-    public void setSpecialization(String specialization) {
-        this.specialization = specialization;
-    }
-
-
     public String getPassword() {
         return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public String getUsername() {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
     @Override
     public void setId(int id) {
-        this.doctor_id = doctor_id;
+        this.id=id;
     }
 
     @Override
     public int getId() {
-        return doctor_id;
+        return id;
     }
+
+
+    private Doctors(DoctorBuilder builder) {
+        this.id = builder.id;
+        this.first_name = builder.first_name;
+        this.last_name = builder.last_name;
+        this.address = builder.address;
+        this.email = builder.email;
+        this.telephone = builder.telephone;
+        this.age = builder.age;
+        this.gender = builder.gender;
+        this.specialization = builder.specialization;
+        this.password = builder.password;
+        this.username = builder.username;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public static class DoctorBuilder{
+        private int id;
+        private final String password;
+        private final String username;
+        private String first_name = " ";
+        private String last_name = " ";
+        private String address = " ";
+        private String email = " ";
+        private String telephone = " ";
+        private int age = 0;
+        private String gender = " ";
+        private String specialization = " ";
+
+        public DoctorBuilder(int id, String username, String password){
+            this.id = id;
+            this.username = username;
+            this.password = password;
+        }
+
+        public DoctorBuilder First_name(String first_name) {
+            this.first_name = first_name;
+            return this;
+        }
+
+        public DoctorBuilder Last_name(String last_name) {
+            this.last_name = last_name;
+            return this;
+        }
+
+        public DoctorBuilder Address(String address) {
+            this.address = address;
+            return this;
+        }
+
+        public DoctorBuilder Email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public DoctorBuilder Telephone(String telephone) {
+            this.telephone = telephone;
+            return this;
+        }
+
+        public DoctorBuilder Age(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public DoctorBuilder Gender(String gender) {
+            this.gender = gender;
+            return this;
+        }
+
+        public DoctorBuilder Specialization(String specialization) {
+            this.specialization = specialization;
+            return this;
+        }
+
+        public Doctors build(){
+            return new Doctors(this);
+        }
+    }
+
 }
