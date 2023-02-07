656dee58841ee447ef198b6ea28ccd652d07db1f
amujkic1 <amujkic1@etf.unsa.ba>
2023-02-07 18:00:33 +0100

    exception to method signature for MyException

--- src/main/java/ba/unsa/etf/rpr/business/RecordManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/RecordManager.java
@@ -2,30 +2,38 @@ package ba.unsa.etf.rpr.business;
 
 import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.domain.Records;
+import ba.unsa.etf.rpr.exceptions.MyException;
+import javafx.collections.ObservableList;
 
 import java.sql.SQLException;
 import java.util.List;
 
 public class RecordManager {
 
-    public List<Records> getAll(){
+    public List<Records> getAll() throws MyException {
         return DaoFactory.recordsDao().getAll();
     }
 
-    public void add(Records rec){
+    public void add(Records rec) throws MyException {
         DaoFactory.recordsDao().add(rec);
     }
 
-    public void update(Records rec) throws SQLException {
+    public void update(Records rec) throws SQLException, MyException {
         DaoFactory.recordsDao().update(rec);
     }
 
-    public Records getById(int id){
+    public Records getById(int id) throws MyException {
         return DaoFactory.recordsDao().getById(id);
     }
-
-    public void delete(int id){
+    public void delete(int id) throws MyException {
         DaoFactory.recordsDao().delete(id);
     }
 
+    public ObservableList<String> allergies(int patientID) throws SQLException{
+        return DaoFactory.recordsDao().allergies(patientID);
+    }
+
+    public ObservableList<String> prescriptions(int patientID) throws SQLException{
+        return DaoFactory.recordsDao().prescriptions(patientID);
+    }
 }
