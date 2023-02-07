f4a4a7fb4d981699e5d1127c84aff6275ba7e9f8
amujkic1 <amujkic1@etf.unsa.ba>
2023-02-07 18:03:16 +0100

    added exception to method signature for MyException

--- src/main/java/ba/unsa/etf/rpr/dao/Dao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/Dao.java
@@ -2,19 +2,20 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Doctors;
 import ba.unsa.etf.rpr.domain.Patients;
+import ba.unsa.etf.rpr.exceptions.MyException;
 
 import java.sql.SQLException;
 import java.util.List;
 
 public interface Dao<T> {
 
-    void add(T item);
+    void add(T item) throws MyException;
 
-    void update(T item) throws SQLException;
+    void update(T item) throws MyException;
 
-    T getById(int id);
+    T getById(int id) throws MyException;
 
-    void delete(int id);
+    void delete(int id) throws MyException;
 
-    public List<T> getAll();
+    public List<T> getAll() throws MyException;
 }
