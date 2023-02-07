1f108a8f19031e7cc9a51f38cfaac8f39a45a961
amujkic1 <amujkic1@etf.unsa.ba>
2023-02-07 18:00:47 +0100

    exception to method signature for MyException

--- src/main/java/ba/unsa/etf/rpr/dao/RecordsDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/RecordsDao.java
@@ -2,7 +2,13 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.dao.Dao;
 import ba.unsa.etf.rpr.domain.Records;
+import javafx.collections.ObservableList;
+
+import java.sql.SQLException;
 
 public interface RecordsDao extends Dao<Records> {
+
+    public ObservableList<String> allergies(int patientID) throws SQLException;
+    public ObservableList<String> prescriptions(int patientID) throws SQLException;
 
 }
