cd840c80f878db2fb1695fd8c84629a4e4fce50e
amujkic1 <amujkic1@etf.unsa.ba>
2023-02-07 17:57:20 +0100

    exception to method signatures for MyException

--- src/main/java/ba/unsa/etf/rpr/business/DoctorManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/DoctorManager.java
@@ -2,6 +2,7 @@ package ba.unsa.etf.rpr.business;
 
 import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.domain.Doctors;
+import ba.unsa.etf.rpr.exceptions.MyException;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -15,23 +16,23 @@ public class DoctorManager {
         if(!searchByUsername(username).getPassword().equals(password)) return "wrong password";
         else return "ok";
     }
-public List<Doctors> getAll(){
+public List<Doctors> getAll() throws MyException {
     return DaoFactory.doctorDao().getAll();
 }
 
-public void add(Doctors doc){
+public void add(Doctors doc) throws MyException {
     DaoFactory.doctorDao().add(doc);
 }
 
-public void update(Doctors doc) throws SQLException{
+public void update(Doctors doc) throws SQLException, MyException {
     DaoFactory.doctorDao().update(doc);
 }
 
-public Doctors getById(int id){
+public Doctors getById(int id) throws MyException {
     return DaoFactory.doctorDao().getById(id);
 }
 
-public void delete(int id){
+public void delete(int id) throws MyException {
     DaoFactory.doctorDao().delete(id);
 }
 
