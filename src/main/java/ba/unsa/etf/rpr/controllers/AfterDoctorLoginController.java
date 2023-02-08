de815a05844a14174560ce1f42193403b37793a2
amujkic1 <amujkic1@etf.unsa.ba>
2023-02-08 17:03:00 +0100

    code cleanup

--- src/main/java/ba/unsa/etf/rpr/controllers/AfterDoctorLoginController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AfterDoctorLoginController.java
@@ -25,8 +25,6 @@ import static javafx.scene.layout.Region.USE_PREF_SIZE;
 
 public class AfterDoctorLoginController implements Initializable {
     @FXML
-    private BorderPane bp;
-    @FXML
     private Button search;
     @FXML
     private TextField txtfield;
@@ -54,21 +52,16 @@ public class AfterDoctorLoginController implements Initializable {
     private Button logout;
     @FXML
     private Label warning;
-
     public ListView patientList;
-    private Stage stage;
-    private Scene scene;
-    private Parent root;
     String DocUsername;
     int doctor_id;
     public AfterDoctorLoginController(String DocUsername, int doctor_id) {
         this.DocUsername = DocUsername;
         this.doctor_id = doctor_id; }
 
-    public void searchByUser() throws MyException {
+    public void searchByUser() throws MyException, IOException {
         if(txtfield.getText().equals("")){ warning.setText("You have to input data for search"); return; }
 
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/patientrecord.fxml"));
         PatientManager pat = new PatientManager();
         DoctorManager dom = new DoctorManager();
         RecordManager rm = new RecordManager();
@@ -77,42 +70,32 @@ public class AfterDoctorLoginController implements Initializable {
         Records rec = rm.getById(patient.getRecord_id());
 
         PatientRecordController patientRecordController = new PatientRecordController(patient, rec, doctor, "d");
-        fxmlLoader.setController(patientRecordController);
+        newWindow("/fxml/patientrecord.fxml", patientRecordController, 1);
 
-        Parent root = null;
-        try {
-            root = fxmlLoader.load();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        Stage stage = new Stage();
-        stage.setScene(new Scene(root, USE_PREF_SIZE, USE_PREF_SIZE));
-        stage.show();
-        Stage s = (Stage) txtfield.getScene().getWindow();
-        s.close();
     }
 
     public void logOut() throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/sample.fxml"));
-        Parent root = fxmlLoader.load();
-        Stage stage = new Stage();
-        stage.setScene(new Scene(root, USE_PREF_SIZE, USE_PREF_SIZE));
-        stage.show();
-        Stage s = (Stage) logout.getScene().getWindow();
-        s.close();
+        newWindow("/fxml/sample.fxml", null, 1);
     }
 
     public void addPatient() throws IOException {
 
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/addrecord.fxml"));
         AddRecordController apc = new AddRecordController(doctor_id);
-        fxmlLoader.setController(apc);
+        newWindow("/fxml/addrecord.fxml", apc, 0);
+
+    }
+
+    public void newWindow(String file, Object o, int close) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(file));
+        if(o != null)
+            fxmlLoader.setController(o);
         Parent root = fxmlLoader.load();
         Stage stage = new Stage();
         stage.setScene(new Scene(root, USE_PREF_SIZE, USE_PREF_SIZE));
         stage.show();
-        Stage s = (Stage)txtfield.getScene().getWindow();
-        //s.close();
+        Stage s = (Stage) txtfield.getScene().getWindow();
+        if(close==1)
+            s.close();
     }
 
     @FXML
@@ -134,22 +117,16 @@ public class AfterDoctorLoginController implements Initializable {
             } catch (MyException e) {
                 throw new RuntimeException(e);
             }
-            System.out.println(selected);
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/patientrecord.fxml"));
+            //System.out.println(selected);
+
             DoctorManager doctor = new DoctorManager();
             PatientRecordController patientRecordController = new PatientRecordController(selected, rec, doctor.searchByUsername(DocUsername), "d");
-            fxmlLoader.setController(patientRecordController);
-            Parent root = null;
             try {
-                root = fxmlLoader.load();
+                newWindow("/fxml/patientrecord.fxml", patientRecordController, 1);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-            Stage stage = new Stage();
-            stage.setScene(new Scene(root, USE_PREF_SIZE, USE_PREF_SIZE));
-            stage.show();
-            Stage s = (Stage)txtfield.getScene().getWindow();
-            s.close();
+
         });
     }
 }
