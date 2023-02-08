387664147e2bb5c3cba2b2eb4805fedb180de4b1
amujkic1 <amujkic1@etf.unsa.ba>
2023-02-08 17:58:05 +0100

    code cleanup

--- src/main/java/ba/unsa/etf/rpr/controllers/CreateAppointmentController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/CreateAppointmentController.java
@@ -5,11 +5,13 @@ import ba.unsa.etf.rpr.business.DoctorManager;
 import ba.unsa.etf.rpr.domain.Appointments;
 import ba.unsa.etf.rpr.domain.Doctors;
 import ba.unsa.etf.rpr.exceptions.MyException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -25,6 +27,10 @@ public class CreateAppointmentController implements Initializable {
     private ChoiceBox choice;
     @FXML
     private DatePicker datepicker;
+    @FXML
+    private Label error;
+    @FXML
+    private Button cancel;
     int patient;
 
     public CreateAppointmentController(int patient_id){
@@ -35,14 +41,29 @@ public class CreateAppointmentController implements Initializable {
 
         Stage stage = (Stage) okbttn.getScene().getWindow();
 
-        AppointmentManager am = new AppointmentManager();
-        Doctors doc = (Doctors)choice.getValue();
-        LocalDate ld = datepicker.getValue();
-        Appointments app = new Appointments(patient, doc.getId(), ld);
-        am.add(app);
+        try {
+            AppointmentManager am = new AppointmentManager();
+            if(datepicker.getValue()==null || choice.getValue()==null) {
+                error.setText("Please fill every field");
+                System.out.println("You have left empty fields");
+                return;
+            }
+            Doctors doc = (Doctors) choice.getValue();
+            LocalDate ld = datepicker.getValue();
+            Appointments app = new Appointments(patient, doc.getId(), ld);
+            am.add(app);
+        }
+        catch (Exception e){
+            throw new MyException(e.getMessage(), e);
+        }
 
         stage.close();
 
+    }
+
+    public void cancel(ActionEvent event) {
+        Stage stage = (Stage) cancel.getScene().getWindow();
+        stage.close();
     }
 
     @Override
