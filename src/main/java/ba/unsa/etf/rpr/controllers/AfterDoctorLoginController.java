5cbe8e78694990d1992d8807ae831607f0c3eb81
amujkic1 <amujkic1@etf.unsa.ba>
2023-02-08 17:57:50 +0100

    code cleanup

--- src/main/java/ba/unsa/etf/rpr/controllers/AfterDoctorLoginController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AfterDoctorLoginController.java
@@ -13,7 +13,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -70,27 +69,30 @@ public class AfterDoctorLoginController implements Initializable {
         Records rec = rm.getById(patient.getRecord_id());
 
         PatientRecordController patientRecordController = new PatientRecordController(patient, rec, doctor, "d");
-        newWindow("/fxml/patientrecord.fxml", patientRecordController, 1);
+        newWindow("/fxml/patientrecord.fxml", patientRecordController, 1, 0);
 
     }
 
     public void logOut() throws IOException {
-        newWindow("/fxml/sample.fxml", null, 1);
+        newWindow("/fxml/sample.fxml", null, 1, 0);
     }
 
     public void addPatient() throws IOException {
 
         AddRecordController apc = new AddRecordController(doctor_id);
-        newWindow("/fxml/addrecord.fxml", apc, 0);
+        newWindow("/fxml/addrecord.fxml", apc, 0, 0);
 
     }
 
-    public void newWindow(String file, Object o, int close) throws IOException {
+    public void newWindow(String file, Object o, int close, int resizable) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(file));
         if(o != null)
             fxmlLoader.setController(o);
         Parent root = fxmlLoader.load();
         Stage stage = new Stage();
+        if(resizable==0){
+            stage.setResizable(false);
+        }
         stage.setScene(new Scene(root, USE_PREF_SIZE, USE_PREF_SIZE));
         stage.show();
         Stage s = (Stage) txtfield.getScene().getWindow();
@@ -117,12 +119,11 @@ public class AfterDoctorLoginController implements Initializable {
             } catch (MyException e) {
                 throw new RuntimeException(e);
             }
-            //System.out.println(selected);
 
             DoctorManager doctor = new DoctorManager();
             PatientRecordController patientRecordController = new PatientRecordController(selected, rec, doctor.searchByUsername(DocUsername), "d");
             try {
-                newWindow("/fxml/patientrecord.fxml", patientRecordController, 1);
+                newWindow("/fxml/patientrecord.fxml", patientRecordController, 1, 0);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
