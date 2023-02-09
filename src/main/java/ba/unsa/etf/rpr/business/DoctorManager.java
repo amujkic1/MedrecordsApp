9a27d8802499f5e1a81caf062f7337c2f60e157e
amujkic1 <amujkic1@etf.unsa.ba>
2023-02-09 13:30:06 +0100

    code cleanup

--- src/main/java/ba/unsa/etf/rpr/business/DoctorManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/DoctorManager.java
@@ -8,7 +8,6 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class DoctorManager {
-
     public String validateDoctorLogin(String username, String password){
         if(username==null) return "empty user field";
         if(password==null) return "empty password field";
@@ -16,25 +15,25 @@ public class DoctorManager {
         if(!searchByUsername(username).getPassword().equals(password)) return "wrong password";
         else return "ok";
     }
-public List<Doctors> getAll() throws MyException {
-    return DaoFactory.doctorDao().getAll();
-}
+    public List<Doctors> getAll() throws MyException {
+        return DaoFactory.doctorDao().getAll();
+    }
 
-public void add(Doctors doc) throws MyException {
-    DaoFactory.doctorDao().add(doc);
-}
+    public void add(Doctors doc) throws MyException {
+        DaoFactory.doctorDao().add(doc);
+    }
 
-public void update(Doctors doc) throws SQLException, MyException {
-    DaoFactory.doctorDao().update(doc);
-}
+    public void update(Doctors doc) throws SQLException, MyException {
+        DaoFactory.doctorDao().update(doc);
+    }
 
-public Doctors getById(int id) throws MyException {
-    return DaoFactory.doctorDao().getById(id);
-}
+    public Doctors getById(int id) throws MyException {
+        return DaoFactory.doctorDao().getById(id);
+    }
 
-public void delete(int id) throws MyException {
-    DaoFactory.doctorDao().delete(id);
-}
+    public void delete(int id) throws MyException {
+        DaoFactory.doctorDao().delete(id);
+    }
 
 public Doctors searchByUsername(String username){
     return DaoFactory.doctorDao().searchByUsername(username);
