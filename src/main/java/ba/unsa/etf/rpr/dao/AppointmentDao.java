516921f150e6586405d775cad9ed2c162e367159
amujkic1 <amujkic1@etf.unsa.ba>
2023-02-09 13:27:46 +0100

    searchByPatient function

--- src/main/java/ba/unsa/etf/rpr/dao/AppointmentDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/AppointmentDao.java
@@ -2,5 +2,10 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Appointments;
 
+import java.sql.SQLException;
+
 public interface AppointmentDao extends Dao<Appointments>{
+
+    Appointments searchByPatient(int patient_id) throws SQLException;
+
 }
