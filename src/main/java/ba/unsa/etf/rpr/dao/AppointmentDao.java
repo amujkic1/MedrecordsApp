2422aaf424f81f2d668373baa880780a5a10348e
amujkic1 <amujkic1@etf.unsa.ba>
2023-02-10 13:12:20 +0100

    added haveAppointmentAtDoctor method

--- src/main/java/ba/unsa/etf/rpr/dao/AppointmentDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/AppointmentDao.java
@@ -1,11 +1,13 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Appointments;
+import javafx.collections.ObservableList;
 
 import java.sql.SQLException;
 
 public interface AppointmentDao extends Dao<Appointments>{
 
     Appointments searchByPatient(int patient_id) throws SQLException;
+    public ObservableList<String> haveAppointmentAtDoctor(int doctor_id) throws SQLException;
 
 }
