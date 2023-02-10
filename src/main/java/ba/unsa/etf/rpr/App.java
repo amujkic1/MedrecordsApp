0ffe87023005dcb746246eaa51b54c1d1cafe386
amujkic1 <amujkic1@etf.unsa.ba>
2023-02-10 12:06:47 +0100

    command line interface

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,37 +1,89 @@
 package ba.unsa.etf.rpr;
 
 import ba.unsa.etf.rpr.business.DoctorManager;
-import ba.unsa.etf.rpr.dao.*;
-import ba.unsa.etf.rpr.domain.Doctors;
+import ba.unsa.etf.rpr.business.PatientManager;
+import ba.unsa.etf.rpr.business.RecordManager;
+import ba.unsa.etf.rpr.domain.Appointments;
 import ba.unsa.etf.rpr.domain.Patients;
-import ba.unsa.etf.rpr.exceptions.MyException;
+import ba.unsa.etf.rpr.domain.Records;
+import org.apache.commons.cli.*;
 
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.sql.Date;
-import java.util.Properties;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
 
-        /*Properties database_prop = new Properties();
-        InputStream resource = null;
-        try {
-            resource = App.class.getResource("/database.properties").openStream();
-            database_prop.load(resource);
-            System.out.println(database_prop.getProperty("username"));
-            System.out.println(database_prop.getProperty("password"));
+    private static final Option appointments = new Option("doc", "get-doctors", false, "Print all doctors");
+    private static final Option findRecord = new Option("rec", "get-record", false, "Find record by patient id");
+    private static final Option addRecord = new Option("addr", "add-record", false, "Add new record");
 
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }*/
+    public static void printFormattedOptions(Options options) {
+        HelpFormatter helpFormatter = new HelpFormatter();
+        PrintWriter printWriter = new PrintWriter(System.out);
+        helpFormatter.printUsage(printWriter, 150, "java -jar medrecords-cli-jar-with-dependencies.jar [option] 'something else if needed'");
+        helpFormatter.printOptions(printWriter, 150, options, 2, 7);
+        printWriter.close();
     }
+
+    public static Options addOptions(){
+        Options options = new Options();
+        options.addOption(appointments);
+        options.addOption(findRecord);
+        options.addOption(addRecord);
+        return options;
+    }
+
+    public static void main(String[] args) throws Exception{
+        Options options = addOptions();
+        CommandLineParser commandLineParser = new DefaultParser();
+        CommandLine cl = commandLineParser.parse(options, args);
+
+        DoctorManager dm = new DoctorManager();
+
+        if((cl.hasOption(appointments.getOpt()) || cl.hasOption(appointments.getLongOpt())) && cl.hasOption((appointments.getLongOpt()))){
+            List<Appointments> appointments = new ArrayList<>();
+
+            try{
+                dm.getAll().forEach(a -> System.out.println(a.getUsername()));
+            }catch (Exception e){
+                System.out.println("No doctors");
+                System.exit(1);
+            }
+        }else if(cl.hasOption(findRecord.getOpt()) || cl.hasOption(findRecord.getLongOpt())){
+
+            try {
+                String username = cl.getArgList().get(0);
+                RecordManager recordManager = new RecordManager();
+                Records record = recordManager.findUserRecord(username);
+                System.out.println(record.getDiagnosis());
+            }catch (Exception e){
+                System.out.println("No record");
+            }
+
+        }else if(cl.hasOption(addRecord.getOpt()) || cl.hasOption(addRecord.getLongOpt())){
+
+            try {
+                String username = cl.getArgList().get(0);
+                Double height = Double.valueOf(cl.getArgList().get(1));
+                Double weight = Double.valueOf(cl.getArgList().get(2));
+                String blood = cl.getArgList().get(3);
+                RecordManager recordManager = new RecordManager();
+                PatientManager patientManager = new PatientManager();
+                Patients patient = patientManager.findByUsername(username);
+                Records record = new Records(patient.getId(), 1, " ", " ", " ", height, weight, blood);
+                recordManager.add(record);
+                System.out.println("Added successfully");
+            }catch (Exception e){
+                System.out.println("Something went wrong with record");
+            }
+
+        }
+        else{
+            printFormattedOptions(options);
+            System.exit(-1);
+        }
+
+    }
+
 }
