7cfe130f0a9ed8eef755a1b54e63891775bed43f
amujkic1 <amujkic1@etf.unsa.ba>
2023-02-11 13:01:20 +0100

    code cleanup

--- src/main/java/ba/unsa/etf/rpr/dao/RecordsDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/RecordsDao.java
@@ -2,13 +2,14 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.dao.Dao;
 import ba.unsa.etf.rpr.domain.Records;
+import ba.unsa.etf.rpr.exceptions.MyException;
 import javafx.collections.ObservableList;
 
 import java.sql.SQLException;
 
 public interface RecordsDao extends Dao<Records> {
-    public Records findUserRecord(String username);
-    public ObservableList<String> allergies(int patientID) throws SQLException;
-    public ObservableList<String> prescriptions(int patientID) throws SQLException;
+    Records findUserRecord(String username) throws MyException;
+    ObservableList<String> allergies(int patientID) throws SQLException;
+    ObservableList<String> prescriptions(int patientID) throws SQLException;
 
 }
