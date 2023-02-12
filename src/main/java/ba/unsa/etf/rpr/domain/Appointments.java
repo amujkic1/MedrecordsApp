4c846de76c0c688f9766dce8cbd4842f1379ef26
amujkic1 <amujkic1@etf.unsa.ba>
2023-02-12 15:34:15 +0100

    code cleanup

--- src/main/java/ba/unsa/etf/rpr/domain/Appointments.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Appointments.java
@@ -1,17 +1,8 @@
 package ba.unsa.etf.rpr.domain;
 
-import java.sql.Date;
 import java.time.LocalDate;
 
 public class Appointments implements Idable{
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
     int id;
     int patient_id;
     int doctor_id;
@@ -28,32 +19,31 @@ public class Appointments implements Idable{
     public int getPatient_id() {
         return patient_id;
     }
-
     public void setPatient_id(int patient_id) {
         this.patient_id = patient_id;
     }
-
     public int getDoctor_id() {
         return doctor_id;
     }
-
     public void setDoctor_id(int doctor_id) {
         this.doctor_id = doctor_id;
     }
-
     public LocalDate getDate() {
         return date;
     }
-
     public void setDate(LocalDate date) {
         this.date = date;
     }
-
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
     @Override
     public void setId(int id) {
         this.id = id;
     }
-
     @Override
     public int getId() {
         return id;
