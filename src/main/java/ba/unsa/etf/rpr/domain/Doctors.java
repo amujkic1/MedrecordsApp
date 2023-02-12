d138ff3cd1a9f8b7b002a6bf6ff0b8649fe4ed17
amujkic1 <amujkic1@etf.unsa.ba>
2023-02-12 15:35:12 +0100

    code cleanup

--- src/main/java/ba/unsa/etf/rpr/domain/Doctors.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Doctors.java
@@ -8,11 +8,6 @@ public class Doctors implements Idable {
     //optional parameters
     private final String first_name;
     private final String last_name;
- //   private final String address;
-   // private final String email;
-   // private final String telephone;
-   // private final int age;
-   // private final String gender;
     private final String specialization;
 
     //public Doctors(){}
@@ -23,10 +18,6 @@ public class Doctors implements Idable {
     public String getLast_name() {
         return last_name;
     }
-    //public String getAddress() {return address;}
-    //public String getEmail() {return email;}
-    //public String getTelephone() {return telephone;}
-    //public int getAge() {return age;}
     public String getSpecialization() {
         return specialization;
     }
@@ -53,17 +44,10 @@ public class Doctors implements Idable {
         this.id = builder.id;
         this.first_name = builder.first_name;
         this.last_name = builder.last_name;
-        //this.address = builder.address;
-        //this.email = builder.email;
-        //this.telephone = builder.telephone;
-        //this.age = builder.age;
-        //this.gender = builder.gender;
         this.specialization = builder.specialization;
         this.password = builder.password;
         this.username = builder.username;
     }
-
-    //public String getGender() {return gender;}
 
     public static class DoctorBuilder{
         private int id;
@@ -71,11 +55,6 @@ public class Doctors implements Idable {
         private final String username;
         private String first_name = " ";
         private String last_name = " ";
-        //private String address = " ";
-        //private String email = " ";
-        //private String telephone = " ";
-        //private int age = 0;
-        //private String gender = " ";
         private String specialization = " ";
 
         public DoctorBuilder(int id, String username, String password){
@@ -93,31 +72,6 @@ public class Doctors implements Idable {
             this.last_name = last_name;
             return this;
         }
-
-        /*public DoctorBuilder Address(String address) {
-            this.address = address;
-            return this;
-        }
-
-        public DoctorBuilder Email(String email) {
-            this.email = email;
-            return this;
-        }
-
-        public DoctorBuilder Telephone(String telephone) {
-            this.telephone = telephone;
-            return this;
-        }
-
-        public DoctorBuilder Age(int age) {
-            this.age = age;
-            return this;
-        }
-
-        public DoctorBuilder Gender(String gender) {
-            this.gender = gender;
-            return this;
-        }*/
 
         public DoctorBuilder Specialization(String specialization) {
             this.specialization = specialization;
