34d31075a81a896006affc35c356be7242b18a97
amujkic1 <amujkic1@etf.unsa.ba>
2023-02-12 17:55:09 +0100

    added javadoc documentation

--- src/main/java/ba/unsa/etf/rpr/dao/PatientsDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/PatientsDao.java
@@ -5,6 +5,11 @@ import javafx.collections.ObservableList;
 
 import java.sql.SQLException;
 
+/**
+ * Dao interface for Appointment domain bean
+ *
+ * @author Ajna Mujkic
+ */
 public interface PatientsDao extends Dao<Patients> {
     Patients findByUsername(String username);
     ObservableList<Patients> allPatients(int docId) throws SQLException;
