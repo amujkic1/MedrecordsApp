b712b92f43c7abd5ec788a29fd3445f786cdf2b1
amujkic1 <amujkic1@etf.unsa.ba>
2023-02-12 17:53:08 +0100

    added javadoc documentation

--- src/main/java/ba/unsa/etf/rpr/dao/DoctorsDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/DoctorsDao.java
@@ -2,6 +2,11 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Doctors;
 
+/**
+ * Dao interface for Doctor domain bean
+ *
+ * @author Ajna Mujkic
+ */
 public interface DoctorsDao extends Dao<Doctors> {
     Doctors searchByUsername(String username);
 
